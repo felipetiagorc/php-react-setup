bafd50a5f690154f9413b44664dd6e8ad193b6a3
Felipe <felipetiagorc@yahoo.com.br>
2020-11-20 01:46:47 -0300

    consertado caminho do build

--- app/_devapp/app.js
+++ app/_devapp/app.js
@@ -2,41 +2,51 @@ import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom';
 import { asyncComponent } from 'react-async-component';
 
-/** We are importing our index.php my app Vairaible */
+/** Aqui importa o index.php onde tem uma variavel chamada myApp
+ * dentro do <script>:     
+ * 
+ * var myApp = {
+        user: <?php echo json_encode($user); ?>,
+        logged: <?php echo $user->logged; ?>
+  */
 import myApp from 'myApp';
 
 /* globals __webpack_public_path__ */
 __webpack_public_path__ = `${window.STATIC_URL}/app/assets/bundle/`;
 
 const Header = asyncComponent({
-    resolve: () => new Promise(resolve =>
-        require.ensure([], require => {
-            resolve(require('./Header'));
+  resolve: () =>
+    new Promise(resolve =>
+      require.ensure(
+        [],
+        require => {
+          resolve(require('./Header'));
         },
-        'Header')
-    )
+        'Header'
+      )
+    ),
 });
 
 class Myapp extends Component {
-    render() {
+  render() {
+    const {
+      user: { name, email },
+      logged,
+    } = myApp;
 
-        const { user : { name, email }, logged } = myApp;
+    return (
+      <Fragment>
+        <Header />
+        <div className='dashboard'>
+          {logged && <h2 className='status'>Logged In</h2>}
+          <h1 className='name'> {name}</h1>
+          <p className='email'>{email}</p>
 
-        return (
-            <Fragment>
-                <Header/>
-                <div className="dashboard">
-                    {logged &&
-                        <h2 className="status">Logged In</h2>
-                    }
-                    <h1 className="name"> {name}</h1>
-                    <p className="email">{email}</p>
-
-                    <p>API host variable {__API_HOST__}</p>
-                </div>
-            </Fragment>
-        )
-    }
+          <p>API host variable {__API_HOST__}</p>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-render(<Myapp/>, document.getElementById('app'));
+render(<Myapp />, document.getElementById('app'));
